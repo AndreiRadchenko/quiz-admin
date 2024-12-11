76f835639659cf7c5e551dc658e5d3c80d5f4d02
andreiradchenko <d4100kx@gmail.com>
2024-12-11 11:08:51 +0200

    mend

--- src/app/[lang]/quiz/layout.tsx
+++ src/app/[lang]/quiz/layout.tsx
@@ -1,14 +1,7 @@
-import { Nav, NavLink } from '@/components/nav/Nav';
-import { IoMdHome } from 'react-icons/io';
-
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/components/nav/app-sidebar';
 import { type Locale } from '../../../../i18n-config';
 
-import {
-  getDictionary,
-  type MenuType,
-} from '../../../../dictionaries/dictionaries';
+import { getDictionary } from '../../../../dictionaries/dictionaries';
 
 export const dynamic = 'force-dynamic';
 
@@ -41,26 +34,6 @@ export default async function QuizLayout({
         menu={menu}
         lang={lang as Locale}
       />
-
-      {/* <Nav menu={menu}>
-        <NavLink href={`/${lang}/quiz`}>
-          <IoMdHome size={24} />
-        </NavLink>
-        <NavLink href={`/${lang}/quiz/players`}>{nav.players}</NavLink>
-        <NavLink href={`/${lang}/quiz/questions`}>{nav.questions}</NavLink>
-        <NavLink href={`/${lang}/quiz/question-definitions`}>
-          {nav.questionDefinitions}
-        </NavLink>
-        <NavLink href={`/${lang}/quiz/answers`}> {nav.answers} </NavLink>
-        <div className="w-10" />
-        <NavLink
-          href={`/${lang}/overview`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {nav.overview}
-        </NavLink>
-      </Nav> */}
       <div className="bg-background container py-6 h-fit">{children}</div>
     </>
   );
